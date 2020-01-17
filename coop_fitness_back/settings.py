eeb6d1f1197f5f31a1d0017e7ca0902f3eb2ab21
Tristan <tristan.alfred@sa-cim.fr>
2020-01-17 16:29:25 +0100

    1er tests

--- coop_fitness_back/settings.py
+++ coop_fitness_back/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_jenkins',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,9 @@ JENKINS_TASKS = (
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+          'rest_framework.authentication.TokenAuthentication',
+    )
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #         'rest_framework.authentication.BasicAuthentication',
     #         'rest_framework.authentication.SessionAuthentication',
