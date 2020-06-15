d8b23252292132a9de4ec980c641d7ed482b429d
tristan <tristanlp.alfred@gmail.com>
2020-06-15 19:37:47 +0200

    Ajout de la documentation des APIs (drf-yasg)

--- coop_fitness_back/urls.py
+++ coop_fitness_back/urls.py
@@ -19,7 +19,22 @@ from rest_framework import routers
 from connection_front import views
 from django.conf import settings, urls
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'users', views.UserViewSet, base_name='utilisateurs')
@@ -46,6 +61,9 @@ urlpatterns = [
              views.RefuseInvitationAPIView.as_view()),
     urls.url('^groupe/(?P<groupe_id>.+)/rend-responsable/(?P<membre_id>.+)$', views.RendResponsableAPIView.as_view()),
     urls.url('^groupe/(?P<groupe_id>.+)/retire-responsable/(?P<membre_id>.+)$', views.RetireResponsableAPIView.as_view()),
+    urls.url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    urls.url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    urls.url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
