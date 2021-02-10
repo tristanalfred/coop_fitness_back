8edfd5531810e8bd62e745e227aa6254fcd3c04f
tristanalfred <43750348+tristanalfred@users.noreply.github.com>
2021-02-10 16:27:20 +0100

    Update test_jenkins.py

--- connection_front/tests/test_jenkins.py
+++ connection_front/tests/test_jenkins.py
@@ -37,17 +37,17 @@ class QuestionMethodTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
 
-def create_question(question_text, days):
-    """
-    Creates a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    def create_question(question_text, days):
+        """
+        Creates a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
 
-def echec_test_volontaire(self):
-    """
-    Zut
-    """
-    self.assertEqual(0, 1)
+    def echec_test_volontaire(self):
+        """
+        Zut
+        """
+        self.assertEqual(0, 1)
